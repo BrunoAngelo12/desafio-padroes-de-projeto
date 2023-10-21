18856eda877d665d618ad333e93989352ef17a41
Brn <bruno.trabalhos@outlook.com>
2023-10-21 16:15:45 -0300

    criado as ações disponíveis do liquidificador usando o conceito de Strategy

---
+++ src/strategy/Grind.java
@@ -0,0 +1,10 @@
+package strategy;
+
+public class Grind implements BlenderModes {
+
+    @Override
+    public void action() {
+        System.out.println("Grinding ingredients...");
+    }
+    
+}
---
+++ src/strategy/Mix.java
@@ -0,0 +1,10 @@
+package strategy;
+
+public class Mix implements BlenderModes {
+
+    @Override
+    public void action() {
+        System.out.println("Mixing ingredients...");
+    }
+    
+}
